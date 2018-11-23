1a742b11aecf5ae3e6472282fd3b60fc5c839e1e
tobi5519 <tobi5519@stud.kea.dk>
2018-11-23 09:17:10 +0100

    Initial commit

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-#noob
+from socket import *
+import _thread
+import HTTPHandler
+
+HOST, PORT = '', 5000
+
+listen_socket = socket(AF_INET, SOCK_STREAM)
+listen_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+listen_socket.bind((HOST, PORT))
+listen_socket.listen(1)
+
+def main(client_connection, client_address):
+    request = client_connection.recv(1024)
+
+
+
+while True:
+    client_connection, client_address = listen_socket.accept()
+    try:
+        _thread.start_new_thread(main, (client_connection, client_address))
+    except:
+        print("Threading error!")
